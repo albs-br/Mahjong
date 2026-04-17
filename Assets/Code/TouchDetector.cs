15bc40fd8ce8d2ef5d830c72a100e10d267dd607
Andre Baptista <albs_br@yahoo.com.br>
2026-04-17 17:43:07 -0300

    v.0.31.0

--- Assets/Code/TouchDetector.cs
+++ Assets/Code/TouchDetector.cs
@@ -1,63 +1,89 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
 
 public class TouchDetector : MonoBehaviour
 {
-    void OnMouseDown()
+    public void OnTouch(InputAction.CallbackContext context)
     {
-        // This code runs when the sprite is touched or clicked
-        
-        // Debug.Log("Sprite Touched: " + gameObject.name);
-        // Debug.Log("sorting order: " + this.gameObject.GetComponent<Renderer>().sortingOrder);
-        // Debug.Log("IsBlocked: " + this.gameObject.GetComponent<Tile>().IsBlocked);
-
-        var tile = gameObject.GetComponent<Tile>();
-
-        if(tile.IsBlocked)
+        if (context.performed)
         {
-            Debug.Log("IsBlocked");
-            return;
-        }
-
-
-        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
-
-        // Debug.Log($"tile.Index: {tile.Index}");
-        var game = tile.Game; //tile.TileLine.TileFloor.Game;
-
-        if(!tile.IsSelected)
-        {
-            if(game.TileSelected == null)
-            {
-                // Set sprite gray (selected)
-                renderer.color = new Color(0.5f, 0.5f, 0.5f, 1.0f);
-                tile.IsSelected = true;
-                game.TileSelected = tile;
-            }
-            else
-            {
-                if(game.TileSelected.TileType == tile.TileType)
-                {
-                    // Remove both tiles
-                    
-                    // Debug.Log("Remove both tiles");
-                    
-                    // // Call this method to hide/deactivate the GameObject
-                    // this.gameObject.SetActive(false);
-
-                    //Debug.Log("Removing tile " + this.gameObject.GetComponent<Tile>().Index);
-                    this.gameObject.GetComponent<Tile>().Remove();
-
-                    game.TileSelected.Remove();
-                    game.TileSelected = null;
-                }
-            }
-        }
-        else
-        {
-            // Remove gray (unselected)
-            renderer.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-            tile.IsSelected = false;
-            game.TileSelected = null;
+            // Access touch position
+            Vector2 touchPos = context.ReadValue<TouchState>().position;
+            Debug.Log($"Touch Detected at: {touchPos}");
         }
     }
+
+    public void OnTouchAction(InputAction.CallbackContext context)
+    {
+        // Read screen position from the touch
+        Vector2 touchPos = context.ReadValue<Vector2>(); 
+        Debug.Log($"Screen Position: {touchPos}");
+    }
+
 }
+
+
+// using UnityEngine;
+
+// public class TouchDetector : MonoBehaviour
+// {
+//     void OnMouseDown()
+//     {
+//         // This code runs when the sprite is touched or clicked
+        
+//         // Debug.Log("Sprite Touched: " + gameObject.name);
+//         // Debug.Log("sorting order: " + this.gameObject.GetComponent<Renderer>().sortingOrder);
+//         // Debug.Log("IsBlocked: " + this.gameObject.GetComponent<Tile>().IsBlocked);
+
+//         var tile = gameObject.GetComponent<Tile>();
+
+//         if(tile.IsBlocked)
+//         {
+//             Debug.Log("IsBlocked");
+//             return;
+//         }
+
+
+//         SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+//         // Debug.Log($"tile.Index: {tile.Index}");
+//         var game = tile.Game; //tile.TileLine.TileFloor.Game;
+
+//         if(!tile.IsSelected)
+//         {
+//             if(game.TileSelected == null)
+//             {
+//                 // Set sprite gray (selected)
+//                 renderer.color = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+//                 tile.IsSelected = true;
+//                 game.TileSelected = tile;
+//             }
+//             else
+//             {
+//                 if(game.TileSelected.TileType == tile.TileType)
+//                 {
+//                     // Remove both tiles
+                    
+//                     // Debug.Log("Remove both tiles");
+                    
+//                     // // Call this method to hide/deactivate the GameObject
+//                     // this.gameObject.SetActive(false);
+
+//                     //Debug.Log("Removing tile " + this.gameObject.GetComponent<Tile>().Index);
+//                     this.gameObject.GetComponent<Tile>().Remove();
+
+//                     game.TileSelected.Remove();
+//                     game.TileSelected = null;
+//                 }
+//             }
+//         }
+//         else
+//         {
+//             // Remove gray (unselected)
+//             renderer.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+//             tile.IsSelected = false;
+//             game.TileSelected = null;
+//         }
+//     }
+// }
